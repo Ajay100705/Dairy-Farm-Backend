00dbcfdae14934aa8da21fd4db32132daf9f5aa0
Ajay Chandel <chandelajay2812@gmail.com>
2026-06-13 23:12:58 +0530

    Register Animal model.

--- apps/animals/admin.py
+++ apps/animals/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from .models import (
+    Animal, AnimalHealthRecord, AnimalWeightLog,
+    BreedingRecord, CalvingRecord, AnimalNote
+)
 
-# Register your models here.
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    """Admin configuration for Animal model.
+    """
+    
+    list_display = [
+        'tag_number', 'name', 'species', 'breed', 'gender',
+        'farm', 'status', 'is_pregnant', 'lactation_number', 'is_active'
+    ]
+    list_filter = [
+        'species', 'breed', 'gender', 'status', 'is_pregnant',
+        'is_active', 'farm', 'date_of_birth'
+    ]
+    search_fields = ['tag_number', 'name', 'farm__name']
+    list_select_related = ['farm', 'mother', 'father']
+    date_hierarchy = 'date_of_birth'
+    fieldsets = (
+        ('Identification', {
+            'fields': ('tag_number', 'name', 'species', 'breed', 'gender')
+        }),
+        ('Farm', {
+            'fields': ('farm',)
+        }),
+        ('Birth Details', {
+            'fields': ('date_of_birth', 'birth_weight', 'birth_location')
+        }),
+        ('Parentage', {
+            'fields': ('mother', 'father')
+        }),
+        ('Physical Characteristics', {
+            'fields': ('color', 'current_weight', 'height')
+        }),
+        ('Status', {
+            'fields': ('status', 'is_active', 'is_pregnant', 'lactation_number')
+        }),
+        ('Calving', {
+            'fields': ('last_calving_date', 'expected_calving_date')
+        }),
+        ('Purchase', {
+            'fields': ('is_purchased', 'purchase_date', 'purchase_price', 'purchase_from')
+        }),
+        ('Sale', {
+            'fields': ('is_sold', 'sale_date', 'sale_price', 'sale_to')
+        }),
+        ('Image', {
+            'fields': ('image',)
+        }),
+    )
+    
+    actions = ['activate_animals', 'deactivate_animals', 'mark_as_lactating']
+    
+    def activate_animals(self, request, queryset):
+        queryset.update(is_active=True)
+    activate_animals.short_description = 'Activate selected animals'
+    
+    def deactivate_animals(self, request, queryset):
+        queryset.update(is_active=False)
+    deactivate_animals.short_description = 'Deactivate selected animals'
+    
+    def mark_as_lactating(self, request, queryset):
+        queryset.filter(gender='female').update(status='lactating')
+    mark_as_lactating.short_description = 'Mark selected females as lactating'
+
+
