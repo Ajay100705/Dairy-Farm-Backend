aa1bb17505e9889bbf777c34ccaf3001d917075d
Ajay Chandel <chandelajay2812@gmail.com>
2026-06-19 23:16:36 +0530

    import library that use in views

--- apps/animals/views.py
+++ apps/animals/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.db.models import Q, Count, Avg
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your views here.
+from .models import (
+    Animal, AnimalHealthRecord, AnimalWeightLog,
+    BreedingRecord, CalvingRecord, AnimalNote 
+)
+
+from .serializers import (
+    AnimalListSerializer,
+    AnimalDetailSerializer,
+    AnimalCreateSerializer,
+    AnimalUpdateSerializer,
+    AnimalHealthRecordSerializer,
+    AnimalWeightLogSerializer,
+    BreedingRecordSerializer,
+    CalvingRecordSerializer,
+    AnimalNoteSerializer,
+    AnimalStatsSerializer,
+    AnimalPedigreeSerializer,
+)
+
+from apps.accounts.permissions import (
+    IsFarmMember, CanManageAnimals, IsOwner
+)
